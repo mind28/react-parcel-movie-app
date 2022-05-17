e1b13c23b54c50185a995eea8720d6e663fc7e6c
mind28 <96925869+mind28@users.noreply.github.com>
2022-05-17 15:31:11 -0400

    Exercise 3.3

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView } from './comonents/main-view/main-view';
+
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
@@ -8,9 +10,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+        <MainView />
     );
   }
 }
